5c9dda90d8e29c465eea73ca5945f14a5ccac52f
Kondratev Aleksandr <wisp.1991@gmail.com>
2024-10-17 05:09:25 +0300

    Practical task - Collection - count of elements completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -20,12 +24,32 @@ public class Main {
             System.out.println(numbers);
         }
 
+        //Практическое задание - Collection - count of elements
+
+        String[] elements = {"one", "two", "three", "four", "two", "one"};
+
+        Map<Object, Integer> map = countOfElements(elements);
+        System.out.println(map);
+
+
     }
 
     public static Object[] filter(Object[] anyArray, Filter filter) {
         Object[] result = new Object[anyArray.length];
         for (int i = 0; i < anyArray.length; i++) {
             result[i] = filter.apply(anyArray[i]);
+        }
+        return result;
+    }
+
+    public static Map<Object, Integer> countOfElements(Object[] elements) {
+        Map<Object, Integer> result = new HashMap<>();
+        for (Object element : elements) {
+            if (result.containsKey(element)) {
+                result.put(element, result.get(element) + 1);
+            } else {
+                result.put(element, 1);
+            }
         }
         return result;
     }
